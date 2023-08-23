e7868b3c597c42c6fca5b173d7fd8b576e0657ac
Marimar03 <md.acuna@alumno.um.edu.ar>
2023-08-22 23:59:53 -0300

    modifico los diccionarios

--- tests/test_models.py
+++ tests/test_models.py
@@ -18,39 +18,34 @@ import unittest
 class TestBagTiles(unittest.TestCase):
     def test_initialize_tiles(self):
         bag = BagTiles()
-        tiles = bag.tiles
-        tile_count = bag.tile_count
-        tile_scores = bag.tile_scores
 
-        # Verificar que haya 100 fichas en total
-        self.assertEqual(len(tiles), 100)
-
+        # Verifica que haya 100 fichas en total
+        self.assertEqual(len(bag.tiles), 100)
         # Verificar que haya 2 comodines
-        self.assertEqual(tile_count['?'], 2)
+        self.assertEqual(bag.tile_count['?'], 2)
 
-        # Verificar cantidad y valor de las letras según las reglas del Scrabble
-        letter_counts = {
+        #verifica la cantidad de cada letra
+        expected_tile_counts = {
             'A': 12, 'B': 2, 'C': 4, 'CH': 1, 'D': 2, 'E': 12, 'F': 1, 'G': 2, 'H': 2,
             'I': 6, 'J': 1, 'L': 4, 'LL': 1, 'M': 2, 'N': 5, 'Ñ': 1, 'O': 8, 'P': 2,
             'Q': 1, 'R': 5, 'RR': 1, 'S': 6, 'T': 4, 'U': 5, 'V': 1, 'X': 1,
             'Y': 1, 'Z': 1,
-        }
+            }
+        for letter, count in expected_tile_counts.items():
+            self.assertEqual(bag.tile_count[letter], count)
 
-        for letter, count in letter_counts.items():
-            self.assertEqual(tile_count[letter], count)
-            self.assertEqual(tile_scores[letter], self.calculate_scrabble_score(letter))
-
-    def calculate_scrabble_score(self, letter):
-        scrabble_scores = {
+        #verifica el puntaje de cada letra
+        expected_tile_scores = {
             'A': 1, 'B': 3, 'C': 3, 'CH': 5, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4,
             'I': 1, 'J': 8, 'L': 1, 'LL': 8, 'M': 3, 'N': 1, 'Ñ': 8, 'O': 1, 'P': 3,
             'Q': 5, 'R': 1, 'RR': 8, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'X': 8,
             'Y': 4, 'Z': 10,
         }
-        return scrabble_scores[letter]
+        for letter, score in expected_tile_scores.items():
+            self.assertEqual(bag.tile_scores[letter], score)
 
-
-    @patch('random.shuffle') #lo hizo el profe
+    #lo hizo el profe preguntarle por patch
+    @patch('random.shuffle') 
     def test_bag_tiles(self, patch_shuffle):
         bag = BagTiles()
         self.assertEqual(

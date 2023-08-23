ecfc4e2adafc07d6e8987ffe301b31543f518af7
Marimar03 <md.acuna@alumno.um.edu.ar>
2023-08-23 15:43:43 -0300

    cambio de rama

--- tests/test_models.py
+++ tests/test_models.py
@@ -83,5 +83,9 @@ class TestBagTiles(unittest.TestCase):
             7,
         )
 
+class TestPlayer(unittest.TestCase):
+    def test_init(self):
+        player_1 
+
 if __name__ == '__main__':
     unittest.main()

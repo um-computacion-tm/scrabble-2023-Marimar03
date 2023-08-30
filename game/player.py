23e0461ffe8577f194ab561ca618648f84c059e0
Marimar03 <md.acuna@alumno.um.edu.ar>
2023-08-29 23:09:01 -0300

    modifico la clase player

--- game/player.py
+++ game/player.py
@@ -1,4 +1,13 @@
+from game.bagtiles import BagTiles
 class Player:
-    def __init__(self):
+    def _init_(self, score):
+        self.score = score
         self.tiles = []
-        
+    def draw_tiles(self, bag):
+        self.tiles = bag.draw_tiles()
+    def show_score(self):
+        return self.score
+    def get_tiles(self):
+        return self.tiles
+    def play_word(self, word):
+        pass

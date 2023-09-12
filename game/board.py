675c1ddb69f2d80f90da05934d24095d52ac369c
Marimar03 <md.acuna@alumno.um.edu.ar>
2023-09-12 16:42:07 -0300

    added word value method

--- game/board.py
+++ game/board.py
@@ -1,26 +1,19 @@
 from game.cell import Cell
 from game.bagtiles import Tiles
 
-
 class Board:
     def __init__(self):
         self.grid = [
-            [ Cell(1, '') for _ in range(15) ]
+            [Cell(1, '') for _ in range(15)]
             for _ in range(15)
         ]
 
-def calculate_word_value:(self):
-total_value = 0
-for cell in word:
-    letter_value = cell.letter.value
-    total_value += letter_value
-return total_value  
+    def calculate_word_value(self, word):
+        total_value = 0
+        for row in self.grid:
+            for cell in row:
+                if cell.letter:
+                    letter_value = cell.letter.value
+                    total_value += letter_value
+        return total_value
 
-def word_inside_board(self):
-    pass
-
-def word_out_of_board(self):
-    pass
-
-def validate_len_of_word(self, word, location, orientation):
-    pass

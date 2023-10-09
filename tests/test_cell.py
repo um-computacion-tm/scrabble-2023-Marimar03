ba9b24e514ff34b036becce67db1ea86d9d74678
Marimar03 <md.acuna@alumno.um.edu.ar>
2023-10-08 23:29:25 -0300

    letter n word multiplier test and multiplier disable

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -14,30 +14,56 @@ class TestCell(unittest.TestCase):
     def test_add_letter(self):
         cell = Cell(multiplier = 1, multiplier_type = '')
         letter = Tile(letter = 'C', value = 3)
-
         cell.add_letter(letter = letter)
-
         self.assertEqual(cell.letter, letter)
 
 
-    def test_cell_letter_multiplier(self):
+    def test_cell_letter_multiplier(self): #OK
         cell = Cell(multiplier = 2, multiplier_type = 'letter')
         letter = Tile(letter = 'C', value = 3)
         cell.add_letter(letter = letter)
         self.assertEqual(cell.calculate_value(),6,)
         
-    def test_cell_word_multiplier(self):
-        cell_1 = Cell(multiplier = 1,multiplier_type = 'letter')
+    def test_cell_word_multiplier(self): #OK
+        cell_1 = Cell(multiplier = 1, multiplier_type = 'letter')
         cell_1.add_letter(Tile('H', 4)) 
-        cell_2 = Cell(multiplier = 1,multiplier_type = 'letter')
+        cell_2 = Cell(multiplier = 1, multiplier_type = 'letter')
         cell_2.add_letter(Tile('O', 1))
-        cell_3 = Cell(multiplier = 3,multiplier_type = 'word')
+        cell_3 = Cell(multiplier = 3, multiplier_type = 'word')
         cell_3.add_letter(Tile('L', 1))
-        cell_4 = Cell(multiplier = 1,multiplier_type = 'letter')
+        cell_4 = Cell(multiplier = 1, multiplier_type = 'letter')
         cell_4.add_letter(Tile('A', 1))
         word = [cell_1, cell_2, cell_3, cell_4]
         value = Board().calculate_word_value(word)
         self.assertEqual(value,21)
     
+    def test_cell_letter_n_word_multiplier(self): #OK
+        cell_1 = Cell(multiplier = 2, multiplier_type = 'letter')
+        cell_1.add_letter(Tile('H', 4)) 
+        cell_2 = Cell(multiplier = 1, multiplier_type = 'letter')
+        cell_2.add_letter(Tile('O', 1))
+        cell_3 = Cell(multiplier = 3, multiplier_type = 'word')
+        cell_3.add_letter(Tile('L', 1))
+        cell_4 = Cell(multiplier = 1, multiplier_type = 'letter')
+        cell_4.add_letter(Tile('A', 1))
+        word = [cell_1, cell_2, cell_3, cell_4]
+        value=Board().calculate_word_value(word)
+        self.assertEqual(value,33)
+
+    def test_cell_multiplier_desactivate(self): #OK
+        cell_1 = Cell(multiplier=1,multiplier_type='letter')
+        cell_1.add_letter(Tile('H', 4)) 
+        cell_2 = Cell(multiplier=1,multiplier_type='letter')
+        cell_2.add_letter(Tile('O', 1))
+        cell_3 = Cell(multiplier=3,multiplier_type='word')
+        cell_3.add_letter(Tile('L', 1))
+        cell_4 = Cell(multiplier=1,multiplier_type='letter')
+        cell_4.add_letter(Tile('A', 1))
+        word = [cell_1, cell_2, cell_3, cell_4]
+        value = Board().calculate_word_value(word)
+        self.assertEqual(value,21)
+        value2=Board().calculate_word_value(word)
+        self.assertEqual(value2,7)
+
 if __name__ == '__main__':
     unittest.main()
